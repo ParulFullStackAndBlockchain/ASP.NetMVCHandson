1ffea12f6398740dcfd681f5c8b7334eb3ccb6e5
ParulTiwari <parul.tiwari17@gmail.com>
2020-07-23 10:29:01 +0530

    ViewData and ViewBag in mvc

--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -12,8 +12,9 @@ namespace MVCDemo.Controllers
         // than just returning a view.It can return another action method or can return a json result.
         public ActionResult Index()
         {
-            //Storing list of countries inside dynamic property Countries of ViewBag object.
-            ViewBag.Countries =new List<string> {
+            //Storing list of countries inside ViewData object using the key "Countries"
+            //Note : Internally ViewBag properties are stored as name/value pairs in the ViewData dictionary.
+            ViewData["Countries"] =new List<string> {
                 "India",
                 "US",
                 "UK",
@@ -21,17 +22,7 @@ namespace MVCDemo.Controllers
             };
             return View();
         }
-
-        //// O/P of below Action metod - System.Collections.Generic.List`1[System.String]
-        //public List<string> Index()
-        //{
-        //    return new List<string> {
-        //        "India",
-        //        "US",
-        //        "UK",
-        //        "Canada"
-        //    };
-        //}
+      
 
     }
 }
