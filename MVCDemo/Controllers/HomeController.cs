5fb2fcbfa69cef1dceb591f5132bec97b4f2284a
ParulTiwari <parul.tiwari17@gmail.com>
2020-08-16 19:50:37 +0530

    Generating a dropdownlist control in mvc using HTML helpers

--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -3,26 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCDemo.Models;
 
 namespace MVCDemo.Controllers
 {
     public class HomeController : Controller
     {
-        //Why the return type is ActionResult and not ViewResult: A contoller action method can do more
-        // than just returning a view.It can return another action method or can return a json result.
+        //To pass list of Departments from the controller, store them in "ViewBag"
         public ActionResult Index()
         {
-            //Storing list of countries inside ViewData object using the key "Countries"
-            //Note : Internally ViewBag properties are stored as name/value pairs in the ViewData dictionary.
-            ViewData["Countries"] =new List<string> {
-                "India",
-                "US",
-                "UK",
-                "Canada"
-            };
+            // Connect to the database
+            EmployeeContext db = new EmployeeContext();
+            // Retrieve departments, and build SelectList
+            ViewBag.Departments = new SelectList(db.Departments, "Id", "Name");
+
             return View();
         }
-      
+
 
     }
 }
