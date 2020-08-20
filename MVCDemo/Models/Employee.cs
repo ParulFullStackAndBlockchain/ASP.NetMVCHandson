dc2d9900f8c2b7bc53d2deb131e1abaf95926316
ParulTiwari <parul.tiwari17@gmail.com>
2020-08-20 19:41:37 +0530

    Using datatype attribute in asp.net mvc application

--- MVCDemo/Models/Employee.cs
+++ MVCDemo/Models/Employee.cs
@@ -29,35 +29,25 @@ namespace MVCDemo.Models
 
     public class EmployeeMetaData
     {
-        [ScaffoldColumn(false)]
-        public int Id { get; set; }
+        // Display mailto hyperlink
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
 
-        //If you want "FullName" to be displayed as "Full Name", 
-        //use DisplayAttribute or DisplayName attribute.
-        //DisplayName attribute is in System.ComponentModel namespace.
-        //[DisplayAttribute(Name="Full Name")]
-        //[Display(Name = "Full Name")]
-        [DisplayName("Full Name")]
-        public string FullName { get; set; }
-
-        //To get only the date part in a datetime data type
-        //[DisplayFormat(DataFormatString = "{0:d}")]
-        //[DisplayFormatAttribute(DataFormatString="{0:d}")]
-
-        //To get time in 24 hour notation
-        //[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm:ss}")]
-
-        //To get time in 12 hour notation with AM PM
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss tt}")]
-        public DateTime? HireDate { get; set; }
-
-        // If gender is NULL, "Gender not specified" text will be displayed.
-        [DisplayFormat(NullDisplayText = "Gender not specified")]
-        public string Gender { get; set; }
-
-        //If you don't want to display a column use ScaffoldColumn attribute.
-        //This only works when you use @Html.DisplayForModel() helper
-        [ScaffoldColumn(false)]
+        // Display currency symbol. For country specific currency, set 
+        // culture using globalization element in web.config. 
+        // For Great Britain Pound symbol
+        // <globalization culture="en-gb"/>
+        [DataType(DataType.Currency)]
         public int? Salary { get; set; }
+
+        // Generate a hyperlink
+        [DataType(DataType.Url)]
+        public string PersonalWebSite { get; set; }
+
+        // Display only Time Part
+        // [DataType(DataType.Time)]
+        // Display only Date Part
+        [DataType(DataType.Date)]
+        public DateTime? HireDate { get; set; }
     }
 }
