586b50ffd0ee877dac9635f13e08cfebe6d49a10
ParulTiwari <parul.tiwari17@gmail.com>
2020-09-01 12:24:13 +0530

    Authorize and AllowAnonymous action filters in mvc, Actual Commit

--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -6,10 +6,14 @@ using System.Web.Mvc;
 
 namespace MVCDemo.Controllers
 {
-    //[Authorize]
+    //At this point, "Authorize" attribute is applicable for all action methods in the HomeController. So, only 
+    //authenticated users will be able to access SecureMethod() and NonSecureMethod().
+    [Authorize]
     public class HomeController : Controller
     {
-        //[AllowAnonymous]
+        //To allow anonymous access to NonSecureMethod(), apply [AllowAnonymous] attribute. AllowAnonymous attribute is 
+        //used to skip authorization enforced by Authorize attribute. 
+        [AllowAnonymous]
         public ActionResult NonSecureMethod()
         {
             return View();
