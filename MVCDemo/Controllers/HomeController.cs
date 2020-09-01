315c9941a2dbcbdd6b3979701b121adb5d78866d
ParulTiwari <parul.tiwari17@gmail.com>
2020-09-01 12:14:38 +0530

    Authorize and AllowAnonymous action filters in mvc

--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using MVCDemo.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,36 +6,18 @@ using System.Web.Mvc;
 
 namespace MVCDemo.Controllers
 {
+    //[Authorize]
     public class HomeController : Controller
     {
-        private SampleDbContext db = new SampleDbContext();
-        
-        public ActionResult Index()
+        //[AllowAnonymous]
+        public ActionResult NonSecureMethod()
         {
-            return View(db.Employees.ToList());
+            return View();
         }
-
-        [HttpPost]
-        public ActionResult Delete(IEnumerable<int> employeeIdsToDelete)
+       
+        public ActionResult SecureMethod()
         {
-            db.Employees.Where(x => employeeIdsToDelete.Contains(x.ID)).ToList().ForEach(y => db.Employees.Remove(y));
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            return View();
         }
-
-        //Suppose Method() is for doing some internal work, and we don't want it to be invoked using a URL request.
-        //To achieve this, decorate Method2() with NonAction attribute.
-        //Now, if you naviage to URL /Home/Method, you will get an error - The resource cannot be found.
-        //Another way to restrict access to methods in a controller, is by making them private.
-        [NonAction]
-        public string Method()
-        {
-            return "<h1>Method 2 Invoked</h1>";
-        }
-        //Note: In general, it's a bad design to have a public method in a controller that is not an action method. If you 
-        //have any such method for performing business calculations, it should be somewhere in the model and not in the 
-        //controller.
-        //However, if for some reason, if you want to have public methods in a controller and you don't want to treat them
-        //as actions, then use NonAction attribute.
     }
 }
