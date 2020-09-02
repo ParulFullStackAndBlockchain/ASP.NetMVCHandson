08046ef0ac5942838045d780b4522d8a4302ef00
ParulTiwari <parul.tiwari17@gmail.com>
2020-09-02 12:36:30 +0530

    OutputCache attribute in mvc

--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -1,20 +1,138 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using MVCDemo.Models;
 
 namespace MVCDemo.Controllers
-{   
+{
     public class HomeController : Controller
     {
-        //In case you want to handle error in a specific action method and not for all controllers and their action 
-        //methods in the entire application.
-        //Note: This also requires code inside the FilterConfig.RegisterGlobalFilters) method to be commented.
-        //[HandleError]
+        private SampleDbContext db = new SampleDbContext();
+
+        //[OutputCache(Duration = 10)]
         public ActionResult Index()
         {
-            throw new Exception("Something went wrong");
+            //System.Threading.Thread.Sleep(3000);
+            return View(db.Employees.ToList());
+        }
+
+        // Child actions can be used to implement partial caching, 
+        // although not necessary. In this case, even if  the ChildActionOnly
+        // attribue is removed, a portion of the view will be cached as expected
+        [ChildActionOnly]
+        [OutputCache(Duration = 10)]
+        public string GetEmployeeCount()
+        {
+            return "Employee Count = " + db.Employees.Count().ToString() + "@ " + DateTime.Now.ToString();
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Gender,Email")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Home/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Gender,Email")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
