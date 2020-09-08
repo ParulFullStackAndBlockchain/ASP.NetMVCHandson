8558655198d48c5838dccfff35815f50aed04d09
ParulTiwari <parul.tiwari17@gmail.com>
2020-09-08 12:40:32 +0530

    Converting Obtrusive to UnObtrusive Javascript using Jquery

---
+++ MVCDemo/Scripts/CustomJavascript.js
@@ -0,0 +1,11 @@
+﻿//We want to change the backgroundColor of "Save" button on "Edit" view to "Red" on MouseOver and to "Green" on MouseOut.
+
+$(function () {
+    $("#btnSubmit").mouseover(function () {
+        $("#btnSubmit").css("background-color", "red");
+    });
+
+    $("#btnSubmit").mouseout(function () {
+        $("#btnSubmit").css("background-color", "green");
+    });
+});
